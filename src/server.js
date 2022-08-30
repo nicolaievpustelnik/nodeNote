6ce8b170c0d6cb98e36d7e811a4d89445e8544d0
Nicolaiev <nbrito@glamit.com.ar>
2022-08-30 20:15:47 -0300

    Add project NodeNotes

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+
+// Glabal variables
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello world')
+});
+
+// Static files
+
+module.exports = app;
